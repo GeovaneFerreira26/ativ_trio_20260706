0c611ce1ee350cd848f177036440a1274d6728ba
BrunnoRosa <bruno.r.silva7@ba.estudante.senai.br>
2026-07-06 19:56:02 -0300

    Projeto inicial

---
+++ projeto-trio/src/test/java/com/example/projeto_trio/ProjetoTrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_trio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
